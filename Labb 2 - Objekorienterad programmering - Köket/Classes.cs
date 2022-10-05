5e321943c6f89be253d7422cd3bc44f9f9d7451c
GustavAlbert <gustav.gitarrsolo@gmail.com>
2022-10-05 14:13:03 +0200

    Creating classes for variables, correcting spelling errors

--- Labb 2 - Objekorienterad programmering - Köket/Classes.cs
+++ Labb 2 - Objekorienterad programmering - Köket/Classes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,38 @@ namespace Klasser
 
 }
 
-
-namespace Meny
+namespace Variables
 {
+    public class Meny
+    {
+        public void Huvudmeny()
+        {
+            Console.WriteLine
+            ("===== Köket =====\n" +
+            "1. Använd köksapparat\n" +
+            "2. Lägg till Köksapparat\n" +
+            "3. Lista köksapparat\n" +
+            "4. Ta bort köksapparat\n" +
+            "5. Avsluta\n" +
+            "Ange val:");
+        }
 
+        //public void A()
+
+        //public void B()
+
+        //public void C()
+
+        //public void D()
+
+        //public void E()
+
+        //public void F()
+
+        //public void G()
+    }
 }
+
 
 
 
--- Labb 2 - Objekorienterad programmering - Köket/Program.cs
+++ Labb 2 - Objekorienterad programmering - Köket/Program.cs
@@ -1,54 +1,40 @@
-﻿//Meny 
-static void HuvudMeny()
-{
-    Console.WriteLine
-        ("===== Köket =====\n" +
-        "1. Använd köksapparat\n" +
-        "2. Lägg till Köksapparat\n" +
-        "3. Liusta köksapparat\n" +
-        "4. Ta bort köksapparat\n" +
-        "5. Avsluta\n" +
-        "Ange val:");
-}
+﻿using Variables;
 
-static void underMeny()
-{
-    Console.WriteLine
-    ();
-}
+var meny = new Meny();
+
 
 bool Launch = true;
 
 while (Launch = true)
 {
-    HuvudMeny();
-    int userinputmainmenu = Convert.ToInt32(Console.ReadLine());
+    meny.Huvudmeny();
+    int userInputMainMenu = Convert.ToInt32(Console.ReadLine());
     
-    if (userinputmainmenu == 1) 
+    if (userInputMainMenu == 1) 
     {
         Console.Clear();
 
         break;
     }
-    else if (userinputmainmenu == 2)
+    else if (userInputMainMenu == 2)
     {
         Console.Clear();
         break;
     }
-    else if (userinputmainmenu == 3)
+    else if (userInputMainMenu == 3)
     {
         Console.Clear();
         break;
     }
-    else if (userinputmainmenu == 4)
+    else if (userInputMainMenu == 4)
     {
         Console.Clear();
         break;
     }
-    else if (userinputmainmenu == 5)
+    else if (userInputMainMenu == 5)
     {
         Console.Clear();
-        Console.WriteLine("Programmet avlsutades successfully");
+        Console.WriteLine("Programmet avslutades successfully");
         Launch = false;
         break;
     }
@@ -60,4 +46,3 @@ while (Launch = true)
             "Vänligen försök igen.");
     }
 }
-
